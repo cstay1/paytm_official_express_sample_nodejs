8268327eb77e384d1c10e111884c7ee6ec574dd2
devprashant <devprashantdawar@gmail.com>
2017-06-07 10:29:42 +0530

    little changes

--- routes/admin/testtxn.js
+++ routes/admin/testtxn.js
@@ -25,7 +25,7 @@ res.render('testtxn.ejs',{'config' : config});
             }
         }
         console.log(paramarray);
-        paramarray['CALLBACK_URL'] = 'https://datapacks.in/response';  // in case if you want to send callback
+        paramarray['CALLBACK_URL'] = 'http://datapacks.in/response';  // in case if you want to send callback
         console.log(PAYTM_MERCHANT_KEY);
         checksum.genchecksum(paramarray, PAYTM_MERCHANT_KEY, function (err, result) 
         {
